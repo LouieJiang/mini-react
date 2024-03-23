6206f2a95d48163d2bd586c1e490b43b451929e5
jianglei <1021124094@qq.com>
2024-03-23 20:43:57 +0800

    v02 修改React名字

--- v02/vite-runner/App.jsx
+++ v02/vite-runner/App.jsx
@@ -1,6 +1,11 @@
+// js pragma 
+/**
+ * @jsx CReact.createElement
+ * import CReact from './core/React.js';
+ */
 import React from './core/React.js';
 
-const App = <div>app99</div>
+const App = <div>app991</div>
 // function App() {
 //   return <div>app99</div>
 // }
