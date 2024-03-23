a55864ca55deb8026062ce5b304d26fdecd49938
jianglei <1021124094@qq.com>
2024-03-23 17:27:39 +0800

    v01 实现动态创建

--- v01/main.js
+++ v01/main.js
@@ -28,13 +28,45 @@
 //   },
 // };
 
-// const container = document.createElement(element.type);
-// container.id = element.props.id;
-// document.querySelector('#root').append(container);
+// const _container = document.createElement(element.type);
+// _container.id = element.props.id;
+// document.querySelector('#root').append(_container);
 
 // const textNode = document.createTextNode('');
 // textNode.nodeValue = textElement.props.nodeValue;
-// container.append(textNode);
+// _container.append(textNode);
+
+
+// function createTextNode(text) {
+//   return {
+//     type: 'TEXT ELEMENT',
+//     props: {
+//       nodeValue: text,
+//       children: [],
+//     },
+//   };
+// }
+
+// function createElement(type, props, ...children) {
+//   return {
+//     type,
+//     props: {
+//       ...props,
+//       children,
+//     },
+//   };
+// }
+
+// const textElement = createTextNode('app');
+// const App = createElement('div', { id: 'app' }, textElement);
+
+// const _container = document.createElement(App.type);
+// _container.id = App.props.id;
+// document.querySelector('#root').append(_container);
+
+// const textNode = document.createTextNode('');
+// textNode.nodeValue = textElement.props.nodeValue;
+// _container.append(textNode);
 
 
 function createTextNode(text) {
@@ -52,20 +84,26 @@ function createElement(type, props, ...children) {
     type,
     props: {
       ...props,
-      children,
+      children: children.map(child => typeof child === 'object' ? child : createTextNode(child)),
     },
   };
 }
+function render(element, container) {
+  const _container = element.type === 'TEXT ELEMENT'
+    ? document.createTextNode('')
+    : document.createElement(element.type);
 
-const textElement = createTextNode('app');
-const App = createElement('div', { id: 'app' }, textElement);
+  Object.keys(element.props)
+    .filter(key => key !== 'children')
+    .forEach(key => {
+      _container[key] = element.props[key];
+    });
 
-const container = document.createElement(App.type);
-container.id = App.props.id;
-document.querySelector('#root').append(container);
+  const children = element.props.children;
+  children.forEach(child => render(child, _container));
+  container.append(_container);
+}
 
-const textNode = document.createTextNode('');
-textNode.nodeValue = textElement.props.nodeValue;
-container.append(textNode);
-
-
+// const textElement = createTextNode('app');
+const App = createElement('div', { id: 'app' }, 'app22');
+render(App, document.querySelector('#root'));
